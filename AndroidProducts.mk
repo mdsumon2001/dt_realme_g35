c603d8c84bf8e4105e0ccb41648a6e119e30e2d8
RayhanDz <raihanadhienugroho@gmail.com>
2022-07-05 06:23:48 +0000

    Rebase

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/nad_RMX2185.mk
+    $(LOCAL_DIR)/aosp_RMX2185.mk
 	
 COMMON_LUNCH_CHOICES := \
-        nad_RMX2185-eng \
-        nad_RMX2185-user \
-        nad_RMX2185-userdebug
+        aosp_RMX2185-eng \
+        aosp_RMX2185-user \
+        aosp_RMX2185-userdebug
